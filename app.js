54d8d6f80531ebac9a136a2fcd8f4e308b61e76e
hvan307 <hongvan307@gmail.com>
2020-02-23 16:14:49 +0000

    pac eats fries

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ function main() {
   const gridCellCount = height * width
   const grid = document.querySelector('.grid')
   const cells = []
+  const scale1path = []
+
   let eaterPosition = 127
   let scale1Position = 22
 
@@ -11,20 +13,34 @@ function main() {
   const right = width + 1
   const down = height + 1
   const left = width - 1
+  const direction = [up, right, down, left]
+  // SCALE 1 
+  const randomDirection = direction[Math.floor(Math.random() * direction.length)]
+  // const moves = (!cells.contains.classList('eater') || !cells.contains.classList('scale1')) ?
+  //   [up, down] : [left, right]
+  console.log(randomDirection)
+
+
+
 
 
   for (let i = 0; i < gridCellCount; i++) {
     const cell = document.createElement('div')
     cell.classList.add('cell')
+    cell.classList.add('fries')
+    // if (cell.style.display === 'none') {
+    //   cell.style.display = 'block'
+    // } else {
+    //   cell.style.display = 'none'
+    // }
     if (i === eaterPosition) {
+      cell.classList.remove('fries')
       cell.classList.add('eater')
     }
     if (i === scale1Position) {
       cell.classList.add('scale1')
     }
-    if (i !== eaterPosition && i !== scale1Position) {
-      cell.classList.add('fries')
-    }
+
     grid.appendChild(cell)
     cells.push(cell)
   }
@@ -33,8 +49,11 @@ function main() {
       clearInterval(scale1Interval)
       return
     }
+
     cells[scale1Position].classList.remove('scale1')
+    cells[scale1Position].classList.add('fries')
     scale1Position -= 1
+    cells[scale1Position].classList.remove('fries')
     cells[scale1Position].classList.add('scale1')
   }, 100)
 
@@ -43,7 +62,7 @@ function main() {
       if (eaterPosition === cells.length - 1) {
         return
       }
-      //  } if (eaterPosition === width || eaterPosition === ) {
+      //  } if (eaterPosition === width) {
       //   cells[eaterPosition].classList.remove('eater')
       // block pacman from going down a row and make him go off the grid and appear on the opposite end of the row instead
       // eaterPosition -= width
@@ -52,6 +71,7 @@ function main() {
 
       cells[eaterPosition].classList.remove('eater')
       eaterPosition += 1
+      cells[eaterPosition].classList.remove('fries')
       cells[eaterPosition].classList.add('eater')
     } else if (event.key === 'ArrowLeft') {
       if (eaterPosition === 0) {
@@ -59,6 +79,7 @@ function main() {
       }
       cells[eaterPosition].classList.remove('eater')
       eaterPosition -= 1
+      cells[eaterPosition].classList.remove('fries')
       cells[eaterPosition].classList.add('eater')
 
     } else if (event.key === 'ArrowUp') {
@@ -67,6 +88,7 @@ function main() {
       }
       cells[eaterPosition].classList.remove('eater')
       eaterPosition -= width
+      cells[eaterPosition].classList.remove('fries')
       cells[eaterPosition].classList.add('eater')
     } else if (event.key === 'ArrowDown') {
       if (eaterPosition > cells.length - width - 1) {
@@ -74,6 +96,7 @@ function main() {
       }
       cells[eaterPosition].classList.remove('eater')
       eaterPosition += width
+      cells[eaterPosition].classList.remove('fries')
       cells[eaterPosition].classList.add('eater')
     }
   })
@@ -90,24 +113,10 @@ function main() {
     const scale1X = (scale1Position + 1) % width
     const scale1Y = ((scale1Position + 1) - scale1X) / width
     scale1Coordinates.push(scale1X, scale1Y)
+    console.log('scale1 coordinates:', scale1Coordinates)
   }
   coordinates()
   console.log(eaterCoordinates)
-
-  // SCALE 1 
-  let direction = 0
-  function randomDirection() {
-    const moves = (direction === left || direction === right) ?
-      [up, down] : [left, right]
-    return moves[Math.floor(Math.random() * 2)]
-  }
-
-  console.log(randomDirection)
-
-
-
-
-
 
 
 
